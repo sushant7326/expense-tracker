00564f46c4f13760ffbc969e3c2f133058288614
sushant-manjaro-pc <111882164+sushant7326@users.noreply.github.com>
2025-03-28 18:27:17 +0530

    fix: schema - id changed to user_id

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(255) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
