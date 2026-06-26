626fdf2203e56a83d352163492188228a2ff0b6e
Sushant Singh <111882164+sushant7326@users.noreply.github.com>
2026-06-26 21:52:20 +0530

    fix: foreign key reference in transactions table

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE
 CREATE TABLE
     transactions (
         transaction_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-        user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+        user_id UUID NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
         amount INTEGER NOT NULL CHECK (amount > 0),
         expense BOOLEAN NOT NULL,
         transaction_time TIMESTAMP
