70a69bd23763c79b332d2ad406ddc9377f0f7ae3
Sushant Singh <111882164+sushant7326@users.noreply.github.com>
2026-07-01 00:56:14 +0530

    fix: enumerate category

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,39 +1,27 @@
-CREATE TABLE
-    users (
-        user_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-        username VARCHAR(255) UNIQUE NOT NULL,
-        password_hash TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE TABLE
-    transactions (
-        transaction_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-        user_id UUID NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
-        amount INTEGER NOT NULL CHECK (amount > 0),
-        expense BOOLEAN NOT NULL,
-        transaction_time TIMESTAMP
-        WITH
-            TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-            title VARCHAR(100) NOT NULL,
-            description TEXT,
-            category VARCHAR(50),
-            payment_method VARCHAR(30),
-            location VARCHAR(100),
-            created_at TIMESTAMP
-        WITH
-            TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP
-        WITH
-            TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-            CONSTRAINT valid_transaction CHECK (
-                (
-                    expense = TRUE
-                    AND amount > 0
-                )
-                OR (
-                    expense = FALSE
-                    AND amount > 0
-                )
-            )
-    );
+CREATE TABLE categories (
+    name VARCHAR(50) PRIMARY KEY
+);
+INSERT INTO categories (name) 
+VALUES ('food'), ('transport'), ('education');
+
+CREATE TABLE transactions (
+    transaction_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
+    amount INTEGER NOT NULL CHECK (amount > 0),
+    expense BOOLEAN NOT NULL,
+    transaction_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    category VARCHAR(50) REFERENCES categories(name) ON DELETE SET NULL ON UPDATE CASCADE,
+    payment_method VARCHAR(30),
+    location VARCHAR(100),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
